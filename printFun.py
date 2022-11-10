e5000491df78d00f057be14cb659c40ca38e7532
akanshak007 <kaushik.akansha2795@gmail.com>
2022-11-10 20:46:25 +0530

    print2

--- printFun.py
+++ printFun.py
@@ -1,2 +1,3 @@
-def myprintFun(): # function name defined  
-    print(" We are inside function")  
+def myprintFun(x): # function name defined  
+    print(" We are inside function and user input value is") 
+    print(x)
